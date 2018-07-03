ca0dc0d2d7174cd2e5fba8619dc44514f3cc612e
Xuan Nguyen <xnguyenit@gmail.com>
2018-07-03 22:13:57 +0700

    add delete, update issue

--- create_issue/main.go
+++ create_issue/main.go
@@ -14,7 +14,7 @@ import (
 
 const APIURL = "https://api.github.com/repos/xuanit/gopl/issues"
 
-const TOKEN = ""
+const TOKEN = "4e003ad1c8f9f6bfe9902d67ac6491093ffcd3ba"
 
 type GetIssueResult struct {
 	Items []*Issue
@@ -24,7 +24,7 @@ type Issue struct {
 	Number    int
 	HTMLURL   string `json:"html_url"`
 	Title     string `json:"title"`
-	State     string
+	State     string `json:"state"`
 	User      *User
 	CreatedAt time.Time `json:"created_at"`
 	Body      string
@@ -61,6 +61,32 @@ func getIssues() ([]*Issue, error) {
 	return result, nil
 }
 
+func getIssue(number string) (*Issue, error) {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", APIURL+"/"+number, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Authorization", "token "+TOKEN)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("getIssues: %v", resp.Status)
+	}
+
+	var result Issue
+	if err = json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
 func createIssue(title string) error {
 	client := &http.Client{}
 
@@ -88,6 +114,44 @@ func createIssue(title string) error {
 	}
 	resp.Body.Close()
 	return nil
+}
+
+func updateIssue(number string, title, state string) error {
+	client := &http.Client{}
+
+	body := Issue{Title: title, State: state}
+
+	var buf bytes.Buffer
+	err := json.NewEncoder(&buf).Encode(body)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", APIURL+"/"+number, &buf)
+	if err != nil {
+		return err
+	}
+	req.Header.Add("Authorization", "token "+TOKEN)
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		resErr, _ := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		return fmt.Errorf("createIssue: %s", string(resErr))
+	}
+	resp.Body.Close()
+	return nil
+}
+
+func deleteIssue(number string) error {
+	issue, err := getIssue(number)
+	if err != nil {
+		return fmt.Errorf("getting issue %v", err)
+	}
+	return updateIssue(number, issue.Title, "closed")
 }
 
 const (
@@ -119,21 +183,70 @@ func getMessage() (string, error) {
 	return string(msgBytes), nil
 }
 
+const (
+	CREATE = "create"
+	UPDATE = "update"
+	DELETE = "delete"
+	LIST   = "list"
+	VIEW   = "view"
+)
+
 func main() {
-	msg, err := getMessage()
-	if err != nil {
-		log.Fatalf("getting message: %v", err)
-	}
-	err = createIssue(msg)
-	if err != nil {
-		fmt.Printf("create issue : %v", err)
+	cmd := os.Args[1]
+	switch cmd {
+	case CREATE:
+		{
+			msg, err := getMessage()
+			if err != nil {
+				log.Fatalf("getting message: %v", err)
+			}
+			err = createIssue(msg)
+			if err != nil {
+				fmt.Printf("create issue : %v", err)
+			}
+		}
+	case LIST:
+		{
+			issues, err := getIssues()
+			if err != nil {
+				fmt.Printf("getIssues : %v", err)
+			}
+			fmt.Printf("#\ttitle\n")
+			for _, v := range issues {
+				fmt.Printf("%d\t%s\n", v.Number, v.Title)
+			}
+		}
+	case DELETE:
+		{
+			err := deleteIssue(os.Args[2])
+			if err != nil {
+				log.Fatalf("deleting issue: %v", err)
+			}
+		}
+	case UPDATE:
+		{
+			msg, err := getMessage()
+			if err != nil {
+				log.Fatalf("getting message: %v", err)
+			}
+			err = updateIssue(os.Args[2], msg, "")
+			if err != nil {
+				fmt.Printf("updating issue issue : %v", err)
+			}
+		}
+	case VIEW:
+		{
+			issue, err := getIssue(os.Args[2])
+			if err != nil {
+				log.Fatalf("deleting issue: %v", err)
+			}
+			jsonString, err := json.Marshal(issue)
+
+			if err != nil {
+				log.Fatalf("mashaling data: %v", err)
+			}
+			fmt.Printf("issue details:\n%s", jsonString)
+		}
 	}
 
-	issues, err := getIssues()
-	if err != nil {
-		fmt.Printf("getIssues : %v", err)
-	}
-	for _, v := range issues {
-		fmt.Println(v.Title)
-	}
 }
